47473e26dadad336bded010fb72e2f922564a57d
Mariano J. Obarrio Miles <mariano.obarrio@gmail.com>
2015-07-11 19:44:34 +0200

    Actualizado con controlador de estadisticas.

--- controller/controller_quiz.js
+++ controller/controller_quiz.js
@@ -102,34 +102,3 @@ exports.destroy = function (req, res) {
 		res.redirect("/quizes"); // Redirecciona a la lista de preguntas
 	}).catch(function(error){next(error);});
 };
-
-// /quizes/statistics
-/*
- * Nro. de preguntas
- * Nro. de comentarios totales
- * Promedio de comentarios por pregunta
- * Nro. de preguntas sin comentarios
- * Nro. de preguntas con comentarios
- */
-
-exports.statistics = function (req, res) {
-	var stats = {};
-	models.Quiz.count().then(function(count) {
-		stats.npreguntas = count;
-		models.Comment.count().then(function(count) {
-			stats.ncomentarios = count;
-			stats.pcomentarios = Number(stats.ncomentarios / stats.npreguntas).toFixed(2);;
-			models.Quiz.findAll({
-				include: [{ model: models.Comment }]
-			}).then(function(quizes){
-				stats.conComm = 0;
-				stats.sinComm = 0;
-				for(index in quizes){
-					if(quizes[index].Comments.length === 0) stats.sinComm++;
-					else stats.conComm++;
-				}
-				res.render('quizes/statistics', { stats: stats, errors: [] });
-			});
-		});
-    });
-};
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var quizController = require('../controller/controller_quiz');
 var commentController = require('../controller/controller_comment');
 var sessionController = require('../controller/controller_session');
+var statisticsController = require('../controller/controller_statistics');
 
 router.get('/', function(req, res, next) { res.render('index', { title: 'Bienvenido a Quiz', errors: [] }); });
 
@@ -23,13 +24,15 @@ router.post('/quizes/create', 				     quizController.create);
 router.get('/quizes/:quizID(\\d+)/edit', 	     quizController.edit);
 router.put('/quizes/:quizID(\\d+)', 		     quizController.update);
 router.delete('/quizes/:quizID(\\d+)', 		     quizController.destroy);
-router.get('/quizes/statistics', 				 quizController.statistics);
 
 // Rutas a Comments
 router.get('/quizes/:quizID(\\d+)/comments/new', commentController.new); // Accede al formulario de crear comentario, asociado al quiz :id.
 router.post('/quizes/:quizID(\\d+)/comments',    commentController.create); // Crea una entrada en la tabla comments, asociada a :quizId en Quiz
 router.get('/quizes/:quizID(\\d+)/comments/:commentID(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 router.delete('/quizes/:quizID(\\d+)/comments/:commentID(\\d+)', sessionController.loginRequired, commentController.destroy);
+
+// Rutas a estadisticas
+router.get('/quizes/statistics', 				 statisticsController.statistics);
 
 // Ruta a los creditos
 router.get('/author', function(req, res, next) { res.render('author', {autor: 'Mariano J. Obarrio Miles', mail: 'mariano.obarrio@gmail.com', errors: []} ); });
